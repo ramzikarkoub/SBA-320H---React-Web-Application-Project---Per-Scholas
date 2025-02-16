5bb586c445933c86a23dbc48722e3c126ac66965
Ramzi Karkoub <karkoub.ramzi@gmail.com>
2025-02-16 00:33:37 -0500

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import { useState } from "react";
 import "./App.css";
+import Nav from "./components/nav/Nav";
+import Home from "./pages/Home/Home";
+import Cart from "./pages/cart/Cart";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <>Wazauuuup </>;
+  return (
+    <>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Home from "../../pages/Home/Home";
+import Cart from "../../pages/cart/Cart";
+
+export default function Nav() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/cart">Cart</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
