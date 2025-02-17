2c5d55fc378cc986275345b47a5ed3457b8c99f2
Ramzi Karkoub <karkoub.ramzi@gmail.com>
2025-02-17 14:51:33 -0500

    add Cart.jsx funtionalities

--- src/assets/CartContext.jsx
+++ src/assets/CartContext.jsx
@@ -25,7 +25,17 @@ export const CartProvider = ({ children }) => {
 
   const addToCart = (product) => {
     setCart((prevCart) => {
-      const updatedCart = [...prevCart, product];
+      const existingProduct = prevCart.find((item) => item.id === product.id);
+      let updatedCart;
+      if (existingProduct) {
+        updatedCart = prevCart.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+      } else {
+        updatedCart = [...prevCart, { ...product, quantity: 1 }];
+      }
       localStorage.setItem("cart", JSON.stringify(updatedCart));
       return updatedCart;
     });
@@ -33,7 +43,14 @@ export const CartProvider = ({ children }) => {
 
   const removeFromCart = (productId) => {
     setCart((prevCart) => {
-      const updatedCart = prevCart.filter((item) => item.id !== productId);
+      let updatedCart = prevCart
+        .map((item) =>
+          item.id === productId
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
+        )
+        .filter((item) => item.quantity > 0);
+
       localStorage.setItem("cart", JSON.stringify(updatedCart));
       return updatedCart;
     });
--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { CartContext } from "../../assets/CartContext";
+import "./Cart.css";
 
 export default function Cart() {
-  return <div>Cart</div>;
+  const { cart, removeFromCart, addToCart } = useContext(CartContext);
+  console.log(cart);
+  useEffect(() => {
+    console.log("Cart updated:", cart);
+  }, [cart]);
+  return (
+    <div className="cart-container">
+      <h1>Cart</h1>
+      {/* <hr /> */}
+      {cart?.map((item, index) => (
+        <div key={index} className="cart-item">
+          <img src={item.image} alt={item.title} />
+          <div className="item-details">
+            <h3>{item.title}</h3>
+            <p>Price: {item.price}</p>
+            <p>Quantity: {item.quantity}</p>
+          </div>
+          <div className="buttons">
+            <button
+              className="delete-btn"
+              onClick={() => removeFromCart(item.id)}
+            >
+              -
+            </button>
+            <button className="delete-btn" onClick={() => addToCart(item)}>
+              +
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
